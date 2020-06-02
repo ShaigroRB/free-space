5b7b2a2a5170caff0cddcb31922d8316ae564b1c
Robin Boucher <robin.boucher@epita.fr>
2020-06-02 22:05:15 +0200

    💄: add style to log of trash.sh

--- scripts/trash.sh
+++ scripts/trash.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+echo "Starting trash 🗑️ cleaning..." ;
+
 read -p "Cleaning .local/share/Trash? [y/n]" answer ;
 if [ "$answer" == "y" ]; then
     rm -rf ~/.local/share/Trash/* ;
-    echo "Finished cleaning .local/share/Trash" ;
+    echo "Finished cleaning .local/share/Trash ✅" ;
 else
     echo "Not cleaning .local/share/Trash" ;
 fi
+
+echo "Finished trash 🗑️ cleaning." ;
